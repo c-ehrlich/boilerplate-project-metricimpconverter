f31c3ea13aeb6ac995d5a1ed5784b6b45eafaf98
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-27 13:10:55 +0100

    implement API

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,34 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+  const convertHandler = new ConvertHandler();
 
+  app.get("/api/convert", (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    if (initNum === undefined && initUnit === undefined)
+      return res.send("invalid number and unit");
+    if (initNum === undefined)
+      return res.send("invalid number");
+    if (initUnit === undefined)
+      return res.send("invalid unit");
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    const returnString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: returnString,
+    });
+  });
 };
