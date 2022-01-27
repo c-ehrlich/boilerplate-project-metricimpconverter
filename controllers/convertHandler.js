4ad37772ee9044a16fabfb899ae457f724c0385e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-27 11:53:53 +0100

    implement getNum and getUnit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,55 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  const _EXCHANGE = {
+    gal: { unit: "gal", verbose: "gallons", returnUnit: "L", ratio: 3.78541 },
+    l: { unit: "L", verbose: "liters", returnUnit: "gal", ratio: 1 / 3.78541 },
+    lbs: { unit: "lbs", verbose: "pounds", returnUnit: "kg", ratio: 0.453592 },
+    kg: { unit: "kg", verbose: "kilograms", returnUnit: "lbs", ratio: 1 / 0.453592 },
+    mi: { unit: "mi", verbose: "miles", returnUnit: "km", ratio: 1.60934 },
+    km: { unit: "km", verbose: "kilometers", returnUnit: "mi", ratio: 1 / 1.60934 },
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
+  this.getNum = function (input) {
+    const numberPortion = input.toLowerCase().match(/[a-z]*$/).toString();
+    if (numberPortion === "") return 1
+    if (numberPortion.split("/").length > 2) return undefined;
     
-    return result;
+    let number;
+    try {
+      number = eval(numberPortion);
+    } catch (e) {
+      return undefined;
+    }
+    
+    return isNaN(number) ? undefined : number
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getUnit = function (input) {
+    const unit = input.toLowerCase().replace(/[a-z]*$/, '');
+    return _EXCHANGE[unit] ? _EXCHANGE[unit] : undefined;
+  };
+
+  this.getReturnUnit = function (initUnit) {
+    return _EXCHANGE[initUnit] ? _EXCHANGE[initUnit].returnUnit : undefined;
+  };
+
+  this.spellOutUnit = function (unit) {
+    return _EXCHANGE[unit] ? _EXCHANGE[unit].verbose : undefined;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
